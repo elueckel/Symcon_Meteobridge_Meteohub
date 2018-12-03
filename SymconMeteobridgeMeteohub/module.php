0fa9c8918adf7050a2afffc09ea5724a34b65fbd
elueckel <30531515+elueckel@users.noreply.github.com>
2018-12-03 19:59:04 +0100

    Update module.php

--- SymconMeteobridgeMeteohub/module.php
+++ SymconMeteobridgeMeteohub/module.php
@@ -121,7 +121,6 @@ if (!defined('vtBoolean')) {
 		If ($this->ReadPropertyBoolean("Station_ISS") == 1)
 		{
 			//$xml = simplexml_load_file('http://'.$User_Name.':'.$Password.'@'.$Server_Address.'/cgi-bin/livedataxml.cgi');
-			print_r($xml);
 			$THB_XML = $xml->THB;
 			$sourceID = $this->ReadPropertyInteger("SourceID");
 			
